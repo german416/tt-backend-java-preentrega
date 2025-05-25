376f25957ede316f8f8d11aab4cd2e0b20ce8ebd
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-05-25 12:59:55 -0300

    feat(main): add main menu to the main method

--- src/main/java/com/german/preentrega/Main.java
+++ src/main/java/com/german/preentrega/Main.java
@@ -1,17 +1,25 @@
 package com.german.preentrega;
 
+import com.german.preentrega.ui.Menu;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        printMenu();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private static void printMenu() throws Exception {
+        Menu menu = new Menu("SISTEMA DE GESTIÓN - TECHLAB", "", "\nSelecciona una opción: ");
+
+        menu.addOption("Agregar producto", '1', () -> System.out.println("Seleccionaste la opción 1"));
+        menu.addOption("Listar productos", '2', () -> System.out.println("Seleccionaste la opción 2"));
+        menu.addOption("Buscar/Actualizar producto", '3', () -> System.out.println("Seleccionaste la opción 3"));
+        menu.addOption("Eliminar producto", '4', () -> System.out.println("Seleccionaste la opción 4"));
+        menu.addOption("Crear un pedido", '5', () -> System.out.println("Seleccionaste la opción 5"));
+        menu.addOption("Listar pedidos", '6', () -> System.out.println("Seleccionaste la opción 6"));
+        menu.addOption("Salir", '7', () -> System.out.println("Seleccionaste la opción 7"));
+
+        menu.run();
     }
 }
