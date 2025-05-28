4b54fdfbfed9716881cc95e5e416725ab5e8dc06
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-05-27 21:29:34 -0300

    feat(main): add product service and basic functions to handle some menu options

--- src/main/java/com/german/preentrega/Main.java
+++ src/main/java/com/german/preentrega/Main.java
@@ -1,25 +1,59 @@
 package com.german.preentrega;
 
+import com.german.preentrega.models.Product;
+import com.german.preentrega.services.ProductService;
 import com.german.preentrega.ui.Menu;
+
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        printMenu();
-    }
+        ProductService productService = new ProductService();
+        productService.populateRepo();
+        productService.printList();
 
-    private static void printMenu() throws Exception {
         Menu menu = new Menu("SISTEMA DE GESTIÓN - TECHLAB", "", "\nSelecciona una opción: ");
 
-        menu.addOption("Agregar producto", '1', () -> System.out.println("Seleccionaste la opción 1"));
-        menu.addOption("Listar productos", '2', () -> System.out.println("Seleccionaste la opción 2"));
-        menu.addOption("Buscar/Actualizar producto", '3', () -> System.out.println("Seleccionaste la opción 3"));
-        menu.addOption("Eliminar producto", '4', () -> System.out.println("Seleccionaste la opción 4"));
+        // 1 - AGREGAR PRODUCTO
+        menu.addOption("Agregar producto", '1', () -> {
+            Product p = new Product("Espejo de mano",3250.00,22);
+            productService.add(p);
+            System.out.println("\nOperación completada. Presioná Enter para volver al menú...");
+            new Scanner(System.in).nextLine();
+        });
+
+        // 2 - LISTAR PRODUCTOS
+        menu.addOption("Listar productos", '2', () -> {
+            productService.printList();
+            System.out.println("\nOperación completada. Presioná Enter para volver al menú...");
+            new Scanner(System.in).nextLine();
+        });
+
+        // 3 - BUSCAR / ACTUALIZAR PRODUCTOS
+        menu.addOption("Buscar/Actualizar producto", '3', () -> {
+//            productService.edit(2);
+//            System.out.println("\nOperación completada. Presioná Enter para volver al menú...");
+//            new Scanner(System.in).nextLine();
+        });
+
+        // 4 - ELIMINAR PRODUCTO
+        menu.addOption("Eliminar producto", '4', () -> {
+            productService.delete(3);
+            System.out.println("\nOperación completada. Presioná Enter para volver al menú...");
+            new Scanner(System.in).nextLine();
+        });
+
         menu.addOption("Crear un pedido", '5', () -> System.out.println("Seleccionaste la opción 5"));
         menu.addOption("Listar pedidos", '6', () -> System.out.println("Seleccionaste la opción 6"));
-        menu.addOption("Salir", '7', () -> System.out.println("Seleccionaste la opción 7"));
+
+        // 7 - SALIR
+        menu.addOption("Salir", '7', menu::quit);
 
         menu.run();
+
+
     }
+
 }
