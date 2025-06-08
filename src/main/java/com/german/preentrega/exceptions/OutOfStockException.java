0df7257ab4ee1cca9cb615d8ab6b8ae55230e235
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-06-08 20:24:56 -0300

    feat(exceptions): add default message to OutOfStockException

--- src/main/java/com/german/preentrega/exceptions/OutOfStockException.java
+++ src/main/java/com/german/preentrega/exceptions/OutOfStockException.java
@@ -1,7 +1,10 @@
 package com.german.preentrega.exceptions;
 
 public class OutOfStockException extends Exception {
+    public OutOfStockException() {
+        super("Stock insuficiente.");
+    }
     public OutOfStockException(String message) {
-      super(message);
+        super(message);
     }
 }
