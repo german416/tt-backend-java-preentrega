a667fd674762cf4760cf3a80052ac520bba07195
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-06-08 20:14:07 -0300

    refactor: move product print logic from view to service

--- src/main/java/com/german/preentrega/ui/views/ProductListView.java
+++ src/main/java/com/german/preentrega/ui/views/ProductListView.java
@@ -13,15 +13,6 @@ public class ProductListView {
     }
 
     public void run() {
-        ArrayList<Product> products = service.getAll();
-        System.out.println("┌──────────────────────────────────────────────────────────┐");
-        System.out.printf("│ %2s\t%-30s\t%10s\t%6s │%n", "ID", "NOMBRE", "PRECIO", "STOCK");
-        System.out.println("├──────────────────────────────────────────────────────────┤");
-
-        for (Product p : products) {
-            System.out.printf("| %2d\t%-30s\t%10.2f\t%6d |%n", p.getId(), p.getName(), p.getPrice(), p.getStock());
-        }
-
-        System.out.println("└──────────────────────────────────────────────────────────┘");
+        service.print();
     }
 }
