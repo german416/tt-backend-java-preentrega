b834b122e9d65654b0f2f40dff0caa26d7a13ba3
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-06-08 20:44:15 -0300

    style(main): update code comments

--- src/main/java/com/german/preentrega/Main.java
+++ src/main/java/com/german/preentrega/Main.java
@@ -7,19 +7,15 @@ import com.german.preentrega.ui.views.*;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Instancio las dependencias de las vistas...
+        // Instancio todas las clases que voy a necesitar
+        // Los servicios
         ProductService productService = new ProductService();
         OrderService orderService = new OrderService();
         Scanner scanner = new Scanner(System.in);
 
-        // Instancio el menu.
-        Menu menu = new Menu("SISTEMA DE GESTIÓN - TECHLAB", "", "\nSelecciona una opción: ");
-
-        // Instancio las vistas.
+        // Las vistas
         ProductListView productListView = new ProductListView(productService);
         AddProductView addProductView = new AddProductView(scanner, productService);
         DeleteProductView deleteProductView = new DeleteProductView(scanner, productService);
@@ -27,47 +23,46 @@ public class Main {
         OrderListView orderListView = new OrderListView(scanner, orderService);
         AddOrderView addOrderView = new AddOrderView(scanner, orderService, productService);
 
+        // Y el menú
+        Menu menu = new Menu("SISTEMA DE GESTIÓN - TECHLAB", "", "\nSelecciona una opción: ");
+
+        // Precargo algunos productos para la demo
         productService.populateRepo();
 
-        // 1 - AGREGAR PRODUCTO
+        // Cargo las opciones del menú
         menu.addOption("Agregar producto", '1', () -> {
             addProductView.run();
             waitForEnter();
         });
 
-        // 2 - LISTAR PRODUCTOS
         menu.addOption("Listar productos", '2', () -> {
             productListView.run();
             waitForEnter();
         });
 
-        // 3 - BUSCAR / ACTUALIZAR PRODUCTOS
         menu.addOption("Buscar/Actualizar producto", '3', () ->{
             findProductView.run();
             waitForEnter();
         });
 
-        // 4 - ELIMINAR PRODUCTO
         menu.addOption("Eliminar producto", '4', () -> {
             deleteProductView.run();
             waitForEnter();
         });
 
-        // 5 - CREAR PEDIDO
         menu.addOption("Crear un pedido", '5', () -> {
             addOrderView.run();
             waitForEnter();
         });
 
-        // 6 - LISTAR PEDIDOS
         menu.addOption("Listar pedidos", '6', () -> {
             orderListView.run();
             waitForEnter();
         });
 
-        // 7 - SALIR
         menu.addOption("Salir", '7', menu::quit);
 
+        // Ejecuto el menú
         menu.run();
     }
 
