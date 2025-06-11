5d530b50d8d705ccaad0cd47d08b82cf81754817
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2025-06-10 21:08:54 -0300

    refactor(views): replace old order print code with service.print()

--- src/main/java/com/german/preentrega/ui/views/OrderListView.java
+++ src/main/java/com/german/preentrega/ui/views/OrderListView.java
@@ -23,15 +23,7 @@ public class OrderListView {
         ArrayList<Order> orders = service.getAll();
 
         if (orders.size() > 0) {
-            System.out.println("┌──────────────────────────────────────────────────────────┐");
-            System.out.printf("│ %2s\t%-30s\t%10s │%n", "ID", "NOMBRE", "TOTAL");
-            System.out.println("├──────────────────────────────────────────────────────────┤");
-
-            for (Order o : orders) {
-                System.out.printf("| %2d\t%-30s\t%10.2f |%n", o.getId(), o.getLabel(), o.getTotalPrice());
-            }
-
-            System.out.println("└──────────────────────────────────────────────────────────┘");
+            service.print();
         } else {
             System.out.println("NO HAY ORDENES QUE LISTAR. TODAVÍA ;)");
         }
